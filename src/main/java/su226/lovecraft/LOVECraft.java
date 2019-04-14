e4772dae932a45ace65f7ba289fdac361d6d0bd9
su226 <1368981939@qq.com>
2019-04-14 15:04:34 +0800

    Change the version to 2.0

--- src/main/java/su226/lovecraft/LOVECraft.java
+++ src/main/java/su226/lovecraft/LOVECraft.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class LOVECraft {
   public static final String MODID = "lovecraft";
   public static final String NAME = "LOVECraft";
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "2.0";
 
   @EventHandler
   public void preInit(FMLPreInitializationEvent event) {
